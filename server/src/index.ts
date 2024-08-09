6ed367695525dfdf78b0efd5c59717ff90f8fe9c
moabdev <macenamoab3@gmail.com>
2024-08-09 19:04:10 -0300

    feat: create index.ts file with backend  application configuration

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// ROUTE IMPORTS
+import dashboardRoutes from "./routes/dashboardRoutes";
+
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.use("/dashboard", dashboardRoutes)
+
+
+// SERVER
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
+});
+
+// curl http://localhost:8000/hello
