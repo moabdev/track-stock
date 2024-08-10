f747683574e08e99cbe5e7740d67ab9006cdd262
moabdev <macenamoab3@gmail.com>
2024-08-10 01:56:32 -0300

    update backend files

--- server/prisma/migrations/20240808115444_set_change_percentage_as_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "ExpenseSummary" ALTER COLUMN "changePercentage" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "PurchaseSummary" ALTER COLUMN "changePercentage" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "SalesSummary" ALTER COLUMN "changePercentage" DROP NOT NULL;
