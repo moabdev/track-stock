d43e91b30f3f04786cd11076507bf5cc14adb396
moabdev <macenamoab3@gmail.com>
2024-08-18 16:17:51 -0300

    style: Change the style of signup and login pages

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { LoginForm } from "@/components/auth-componentes/LoginForm";
+
+export default function Login() {
+  return (
+    <div className="flex items-center">
+      <LoginForm />
+    </div>
+  );
+}
---
+++ client/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { SignupForm } from "@/components/auth-componentes/SignupForm";
+
+export default function signup() {
+  return <SignupForm />;
+}
